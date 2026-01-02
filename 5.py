ee99665178c52856f3661f41f74cb5f454f79fbe
Zhengyang <songzy_thu@163.com>
2026-01-02 14:11:23 +0800

    Add solution for level 5.

--- 5.py
+++ 5.py
@@ -1,1 +1,47 @@
 # http://www.pythonchallenge.com/pc/def/peak.html
+
+import urllib.request
+import pickle
+
+
+DATA_URL = "http://www.pythonchallenge.com/pc/def/banner.p"
+
+def fetch_data(url):
+    with urllib.request.urlopen(url) as response:
+        return response.read()
+    
+def unpickle_data(data):
+    return pickle.loads(data)
+
+if __name__ == "__main__":
+    data = fetch_data(DATA_URL)
+    banner = unpickle_data(data)
+    print(banner)
+
+    for line in banner:
+        output_line = ''.join(char * count for char, count in line)
+        print(output_line)
+
+"""
+              #####                                                                      ##### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+      ###      ####   ###         ###       #####   ###    #####   ###          ###       #### 
+   ###   ##    #### #######     ##  ###      #### #######   #### #######     ###  ###     #### 
+  ###     ###  #####    ####   ###   ####    #####    ####  #####    ####   ###     ###   #### 
+ ###           ####     ####   ###    ###    ####     ####  ####     ####  ###      ####  #### 
+ ###           ####     ####          ###    ####     ####  ####     ####  ###       ###  #### 
+####           ####     ####     ##   ###    ####     ####  ####     #### ####       ###  #### 
+####           ####     ####   ##########    ####     ####  ####     #### ##############  #### 
+####           ####     ####  ###    ####    ####     ####  ####     #### ####            #### 
+####           ####     #### ####     ###    ####     ####  ####     #### ####            #### 
+ ###           ####     #### ####     ###    ####     ####  ####     ####  ###            #### 
+  ###      ##  ####     ####  ###    ####    ####     ####  ####     ####   ###      ##   #### 
+   ###    ##   ####     ####   ###########   ####     ####  ####     ####    ###    ##    #### 
+      ###     ######    #####    ##    #### ######    ###########    #####      ###      ######
+"""
